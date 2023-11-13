385ecb3af4ef57c0fef585ec9ea867e43ebe7bd9
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-11-13 13:08:08 +0000

    update console server

--- lib/src/services/logger.service.ts
+++ lib/src/services/logger.service.ts
@@ -1,21 +1,28 @@
 import { Console } from '@esliph/console'
 import { Service } from '../common/module'
 
-const TEMPLATE_LOG = '<prefix?value="#"&styles=italic> <dateTime>  <method?background=blue>  <context?color=green&styles=bold>: <message>'
-const TEMPLATE_ERROR = '<prefix?value="#"&styles=italic> <dateTime>  <method?background=red>  <context?color=green&styles=bold>: <message>'
-const TEMPLATE_WARN = '<prefix?value="#"&styles=italic> <dateTime>  <method?background=yellow&color=black>  <context?color=green&styles=bold>: <message>'
-const TEMPLATE_INFO = '<prefix?value="#"&styles=italic> <dateTime>  <method?background=white>  <context?color=green&styles=bold>: <message>'
+const TEMPLATE_LOG =
+    '<prefix?value="#"&styles=italic> <dateTime>  <method?background=blue>  <_?value="["&color=green&styles=bold><context?color=green&styles=bold><-?value="]"&color=green&styles=bold>: <message>'
+const TEMPLATE_ERROR =
+    '<prefix?value="#"&styles=italic> <dateTime>  <method?background=red>  <_?value="["&color=green&styles=bold><context?color=green&styles=bold><-?value="]"&color=green&styles=bold>: <message>'
+const TEMPLATE_WARN =
+    '<prefix?value="#"&styles=italic> <dateTime>  <method?background=yellow&color=black>  <_?value="["&color=green&styles=bold><context?color=green&styles=bold><-?value="]"&color=green&styles=bold>: <message>'
+const TEMPLATE_INFO =
+    '<prefix?value="#"&styles=italic> <dateTime>  <method?background=white>  <_?value="["&color=green&styles=bold><context?color=green&styles=bold><-?value="]"&color=green&styles=bold>: <message>'
 
-@Service({ name: 'logger' })
+@Service({ name: 'global.service.logger' })
 export class Logger extends Console<typeof TEMPLATE_LOG, typeof TEMPLATE_ERROR, typeof TEMPLATE_WARN, typeof TEMPLATE_INFO> {
     constructor(args: { context?: string } = {}) {
         super({
-            methodsValue: { context: args.context ? `[${args.context}]` : '' }, templates: {
+            methodsValue: {
+                context: args.context ? `[${args.context}]` : 'APP',
+            },
+            templates: {
                 log: TEMPLATE_LOG,
                 error: TEMPLATE_ERROR,
                 warn: TEMPLATE_WARN,
                 info: TEMPLATE_INFO,
-            }
+            },
         })
     }
 }
