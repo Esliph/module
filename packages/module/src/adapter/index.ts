4656c4d80af76685b7cd33841ce4d74d6e7c613d
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-12-11 15:41:17 +0000

    update adapter and remove listen native

--- packages/module/src/adapter/index.ts
+++ packages/module/src/adapter/index.ts
@@ -4,5 +4,4 @@ export abstract class Adapter<T = any> {
     abstract adapterKey: string
     abstract get instance(): T
     abstract loadEvent(event: AdapterLoadEventOptions): void
-    abstract listen(args: { port: number }, handler: () => any | void): void
 }
